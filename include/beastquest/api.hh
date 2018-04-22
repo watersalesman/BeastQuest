ce29d49bf8100bf2eaccb87e1523a5aa308bad28
Randy Ramos <rramos1295@gmail.com>
2018-04-22 11:45:13 -0400

    Move setOption to Session::SetOption

--- include/beastquest/api.hh
+++ include/beastquest/api.hh
@@ -1,97 +1,45 @@
 #ifndef BEASTQUEST_API_HH
 #define BEASTQUEST_API_HH
 
-#include "beastquest/auth.hh"
-#include "beastquest/multipart.hh"
 #include "beastquest/response.hh"
 #include "beastquest/session.hh"
-#include "beastquest/simpleform.hh"
-#include "beastquest/url.hh"
 
 #include <future>
-#include <type_traits>
 
 namespace quest {
-
-namespace {
-// Enables if types are the same without references and cv-qualifiers
-template <typename T, typename U>
-using enableIfSameClass_t = typename std::enable_if<
-    std::is_same<T, typename std::decay<U>::type>::value, bool>::type;
-
-// Template overloads for different Session options
-template <typename T, enableIfSameClass_t<Url, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetUrl(std::forward<T>(opt));
-}
-
-template <typename T, enableIfSameClass_t<Headers, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetHeaders(std::forward<T>(opt));
-}
-
-template <typename T, enableIfSameClass_t<Parameters, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetParameters(std::forward<T>(opt));
-}
-
-template <typename T, enableIfSameClass_t<SimpleForm, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetSimpleForm(std::forward<T>(opt));
-}
-
-template <typename T, enableIfSameClass_t<Multipart, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetMultipart(std::forward<T>(opt));
-}
-
-template <typename T, enableIfSameClass_t<BasicAuth, T> = false>
-void setOption(Session& session, T&& opt) {
-  session.SetAuth(std::forward<T>(opt));
-}
-
-// If multiple options are given, set one option, then move on to the next one
-template <typename T, typename... Ts>
-void setOption(Session& session, T&& opt, Ts&&... ts) {
-  setOption(session, std::forward<T>(opt));
-  setOption(session, std::forward<Ts>(ts)...);
-}
-
-}  // namespace
-
 // Synchronous request API
 template <typename... Ts>
 Response Get(Ts&&... ts) {
   Session sess;
-  setOption(sess, std::forward<Ts>(ts)...);
+  sess.SetOption(std::forward<Ts>(ts)...);
   return sess.Get();
 }
 
 template <typename... Ts>
 Response Post(Ts&&... ts) {
   Session sess;
-  setOption(sess, std::forward<Ts>(ts)...);
+  sess.SetOption(std::forward<Ts>(ts)...);
   return sess.Post();
 }
 
 template <typename... Ts>
 Response Delete(Ts&&... ts) {
   Session sess;
-  setOption(sess, std::forward<Ts>(ts)...);
+  sess.SetOption(std::forward<Ts>(ts)...);
   return sess.Delete();
 }
 
 template <typename... Ts>
 Response Put(Ts&&... ts) {
   Session sess;
-  setOption(sess, std::forward<Ts>(ts)...);
+  sess.SetOption(std::forward<Ts>(ts)...);
   return sess.Put();
 }
 
 template <typename... Ts>
 Response Patch(Ts&&... ts) {
   Session sess;
-  setOption(sess, std::forward<Ts>(ts)...);
+  sess.SetOption(std::forward<Ts>(ts)...);
   return sess.Patch();
 }
 
--- include/beastquest/session.hh
+++ include/beastquest/session.hh
@@ -13,6 +13,11 @@
 
 namespace quest {
 
+// Alias that enables if types are the same without references and cv-qualifiers
+template <typename T, typename U>
+using enableIfSameClass_t = typename std::enable_if<
+    std::is_same<T, typename std::decay<U>::type>::value, bool>::type;
+
 class Session {
  public:
   Session();
@@ -33,6 +38,20 @@ class Session {
   void SetMultipart(Multipart&& multipart) noexcept;
   void SetAuth(const BasicAuth& basic_auth) noexcept;
   void SetAuth(BasicAuth&& basic_auth) noexcept;
+  template <typename T, enableIfSameClass_t<Url, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, enableIfSameClass_t<Headers, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, enableIfSameClass_t<Parameters, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, enableIfSameClass_t<SimpleForm, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, enableIfSameClass_t<Multipart, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, enableIfSameClass_t<BasicAuth, T> = false>
+  void SetOption(T&& opt);
+  template <typename T, typename... Ts>
+  void SetOption(T&& opt, Ts&&... ts);
   Response Get();
   Response Post();
   Response Delete();
@@ -43,6 +62,44 @@ class Session {
   class Impl;
   std::unique_ptr<Impl> pimpl_;
 };
+
+// Template overloads for different Session options
+template <typename T, enableIfSameClass_t<Url, T> = false>
+void Session::SetOption(T&& opt) {
+  SetUrl(std::forward<T>(opt));
+}
+
+template <typename T, enableIfSameClass_t<Headers, T> = false>
+void Session::SetOption(T&& opt) {
+  SetHeaders(std::forward<T>(opt));
+}
+
+template <typename T, enableIfSameClass_t<Parameters, T> = false>
+void Session::SetOption(T&& opt) {
+  SetParameters(std::forward<T>(opt));
+}
+
+template <typename T, enableIfSameClass_t<SimpleForm, T> = false>
+void Session::SetOption(T&& opt) {
+  SetSimpleForm(std::forward<T>(opt));
+}
+
+template <typename T, enableIfSameClass_t<Multipart, T> = false>
+void Session::SetOption(T&& opt) {
+  SetMultipart(std::forward<T>(opt));
+}
+
+template <typename T, enableIfSameClass_t<BasicAuth, T> = false>
+void Session::SetOption(T&& opt) {
+  SetAuth(std::forward<T>(opt));
+}
+
+// If multiple options are given, set one option, then move on to the next one
+template <typename T, typename... Ts>
+void Session::SetOption(T&& opt, Ts&&... ts) {
+  SetOption(std::forward<T>(opt));
+  SetOption(std::forward<Ts>(ts)...);
+}
 
 }  // namespace quest
 

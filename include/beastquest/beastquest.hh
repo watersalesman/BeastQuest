0fc462ac7b650349cdb59f88b7af9ec439480582
Randy Ramos <rramos1295@gmail.com>
2018-04-22 13:35:14 -0400

    Add a couple of includes to convenience header

--- include/beastquest/beastquest.hh
+++ include/beastquest/beastquest.hh
@@ -4,9 +4,11 @@
 #include "beastquest/api.hh"
 #include "beastquest/auth.hh"
 #include "beastquest/multipart.hh"
+#include "beastquest/optiontypes.hh"
 #include "beastquest/parameters.hh"
 #include "beastquest/response.hh"
 #include "beastquest/session.hh"
+#include "beastquest/simpleform.hh"
 #include "beastquest/url.hh"
 
 #endif  // BEASTQUEST_HH

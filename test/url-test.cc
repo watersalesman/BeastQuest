360be0463f2ee705ec75038081d9bfb53f0234d4
Randy Ramos <rramos1295@gmail.com>
2018-04-30 13:31:58 -0400

    Add tests for quest::Url::CanShareConnectionWith

--- test/url-test.cc
+++ test/url-test.cc
@@ -33,3 +33,34 @@ TEST(UrlTest, ParseUrl) {
   EXPECT_EQ(url.use_ssl, true);
   EXPECT_EQ(url.port, "4444");
 }
+
+TEST(UrlTest, CannotShareLoopbackConnection) {
+  quest::Url loop_url1("http://localhost");
+  quest::Url loop_url2("http://127.0.0.1");
+  quest::Url other_url("http://example.com");
+
+  EXPECT_FALSE(loop_url1.CanShareConnectionWith(loop_url2));
+  EXPECT_FALSE(loop_url1.CanShareConnectionWith(other_url));
+  EXPECT_FALSE(loop_url2.CanShareConnectionWith(loop_url1));
+  EXPECT_FALSE(loop_url2.CanShareConnectionWith(other_url));
+  EXPECT_FALSE(other_url.CanShareConnectionWith(loop_url1));
+  EXPECT_FALSE(other_url.CanShareConnectionWith(loop_url2));
+}
+
+TEST(UrlTest, CanShareNormalConnection) {
+  quest::Url url1("http://example.com:80/page1");
+  quest::Url url2("http://example.com:80/page2");
+
+  EXPECT_TRUE(url1.CanShareConnectionWith(url2));
+}
+
+TEST(UrlTest, CannotShareNormalConnection) {
+  quest::Url url1("http://example.com:80");
+  quest::Url url2("http://example.com:8080");
+  quest::Url url3("https://example.com");
+  quest::Url url4("http://other-example.com");
+
+  EXPECT_FALSE(url1.CanShareConnectionWith(url2));
+  EXPECT_FALSE(url1.CanShareConnectionWith(url3));
+  EXPECT_FALSE(url1.CanShareConnectionWith(url4));
+}
